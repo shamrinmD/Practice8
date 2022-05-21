238b57fe468cec72971a3024d66c9721e874def1
unknown <shamrin.maks@mail.ru>
2022-05-21 22:13:21 +0300

    First commit

---
+++ viewmodel/src/main/java/ru/mirea/shamrin/viewmodel/MainActivity.java
@@ -0,0 +1,34 @@
+package ru.mirea.shamrin.viewmodel;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progressBar = findViewById(R.id.progressBar);
+
+        ProgressViewModel viewModel =
+                ViewModelProviders.of(this).get(ProgressViewModel.class);
+        viewModel.getProgressState().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(@Nullable Boolean isVisibleProgressBar) {
+                if (isVisibleProgressBar) {
+                    progressBar.setVisibility(View.VISIBLE);
+                } else {
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+        viewModel.showProgress();
+    }
+}
